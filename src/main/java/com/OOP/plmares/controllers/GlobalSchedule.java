4f7580848b274812630ce37a1f3df93cb0ce3795
misyel <misheruvynx@gmail.com>
2024-01-16 21:46:33 +0800

    final commit

--- src/main/java/com/OOP/plmares/controllers/GlobalSchedule.java
+++ src/main/java/com/OOP/plmares/controllers/GlobalSchedule.java
@@ -3,10 +3,11 @@ package com.OOP.plmares.controllers;
 import java.io.*;
 import java.time.LocalDate;
 import java.util.Properties;
-
 public class GlobalSchedule {
-    private static final String FILE_PATH = "src/main/resources/globalSchedule.properties";
+    private static final String FILE_PATH = GlobalSchedule.class.getClassLoader().getResource("globalSchedule.properties").getPath();
+
     private static Properties properties;
+
     static {
         properties = new Properties();
         loadProperties();
@@ -33,14 +34,27 @@ public class GlobalSchedule {
     }
 
     private static void loadProperties() {
-        try (InputStream input = new FileInputStream(FILE_PATH)) {
-            properties.load(input);
+        try (InputStream input = GlobalSchedule.class.getClassLoader().getResourceAsStream("globalSchedule.properties")) {
+            if (input != null) {
+                properties.load(input);
+            } else {
+                createNewFile(); // Create a new file if it doesn't exist
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+
     private static void saveProperties() {
+        try (OutputStream output = new FileOutputStream(FILE_PATH)) {
+            properties.store(output, "Global Schedule");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void createNewFile() {
         try (OutputStream output = new FileOutputStream(FILE_PATH)) {
             properties.store(output, "Global Schedule");
         } catch (IOException e) {
