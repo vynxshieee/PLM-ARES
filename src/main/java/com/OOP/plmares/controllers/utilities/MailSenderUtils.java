032a41871f40643a30bdef31c592d863fb15d61d
Matchi <103355835+vynxshieee@users.noreply.github.com>
2024-01-21 04:26:53 +0800

    Update MailSenderUtils.java

--- src/main/java/com/OOP/plmares/controllers/utilities/MailSenderUtils.java
+++ src/main/java/com/OOP/plmares/controllers/utilities/MailSenderUtils.java
@@ -15,7 +15,7 @@ public class MailSenderUtils {
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "smtp.gmail.com"); // replace with your SMTP server
+        props.put("mail.smtp.host", "smtp.gmail.com"); 
         props.put("mail.smtp.port", "587");
 
         Session session = Session.getInstance(props,
